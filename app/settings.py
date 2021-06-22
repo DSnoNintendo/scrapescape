3b61338673a78c660f183f98a7c67a017c766275
New User <newuser@News-MBP.attlocal.net>
2021-06-21 19:30:45 -0700

    version 1.0.1

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-t9ig@7wohtbjs(ia1kkqleo7122=fyg1wh3p6vs&#c#zn!sw^h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://scrapescape.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 'scrapescape.herokuapp.com',
+                 '.scrapescape.com']
 
 
 # Application definition
